ae5fff531541321104a53f8e98c9fd71a8669caa
Edson Lucas <edson@iMac-de-Thiago.local>
2019-09-10 18:33:03 -0300

    estrutura inicializada

---
+++ src/pages/Main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Title } from '../styles/main';
+
+const Main = () => <Title>Hello World</Title>;
+
+export default Main;
---
+++ src/pages/Repository.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Repository = () => <h1>Hello Repository</h1>;
+
+export default Repository;
