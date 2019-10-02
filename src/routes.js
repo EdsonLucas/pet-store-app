ecffd8dd80d0ee8da6c94ea2e3ab0e5023ab9976
Edson Lucas <contato@edsonlucas.com.br>
2019-10-02 08:34:27 -0300

    adicionado todas as rotas

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import Address from './pages/Address';
+import NewAddress from './pages/NewAddress';
+import About from './pages/About';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
+      <Route path="/login" component={Login} />
       <Route path="/" exact component={Dashboard} />
+      <Route path="/product" component={Product} />
+      <Route path="/cart" component={Cart} />
+      <Route path="/address" component={Address} />
+      <Route path="/new-address" component={NewAddress} />
+      <Route path="/about" component={About} />
     </Switch>
   </BrowserRouter>
 );
